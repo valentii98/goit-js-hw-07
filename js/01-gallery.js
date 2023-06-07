9f3cb638ec4ca8af7c7a20b64a13f36a65ca88f2
Bohdan Valentii <b.valentii98@gmail.com>
2023-06-07 21:44:52 +0300

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const createGalleryItem = ({ preview, original, description }) => `
+<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>
+`;
+
+galleryList.innerHTML = galleryItems.map(createGalleryItem).join("");
+
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+  const modalImg = event.target
+    .closest(".gallery__image")
+    .getAttribute("data-source");
+  const instance = basicLightbox.create(`
+      <img src="${modalImg}" width="800" height="600">
+    `);
+  instance.show();
+  document.addEventListener("keydown", (event) => onEscPress(event, instance));
+});
+
+const onEscPress = (event, instance) => {
+  const ESC_KEYCODE = "Escape";
+  if (event.code === ESC_KEYCODE) {
+    instance.close();
+    document.removeEventListener("keydown", (event) =>
+      onEscPress(event, instance)
+    );
+  }
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+const createGalleryItem = ({ preview, original, description }) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}" data-original-img=${original}>
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>
+`;
+galleryList.innerHTML = galleryItems.map(createGalleryItem).join("");
+// galleryList.addEventListener("click", (event) => {
+//
+// });
+const lightbox = new SimpleLightbox(".gallery__link", {
+  animationSpeed: 250,
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
+lightbox.on("show.simplelightbox");
